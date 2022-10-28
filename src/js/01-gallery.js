37912bc2822a07401c23231efbaf2b7fe9d6a51d
Yev1999 <92596319+Yev1999@users.noreply.github.com>
2022-10-28 11:39:30 +0300

    Gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const renderElements = (galleryItems) => {
+  let refsGallery = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+  gallery.insertAdjacentHTML("afterbegin", refsGallery);
+};
+
+const onGalleryClick = (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const img = e.target;
+  const imgURL = img.dataset.source;
+  const instance = basicLightbox.create(`<img src="${imgURL}" />`);
+  instance.show();
+};
+
+renderElements(galleryItems);
+gallery.addEventListener("click", onGalleryClick);
